ae07ffb098fb60cca71f2ddfc172d75115d74492
Aapo Poutanen <aapo.poutanen@aalto.fi>
2018-04-12 01:01:19 +0300

    better structures, cleaner

--- pygtpnl/__init__.py
+++ pygtpnl/__init__.py
@@ -1,5 +1,4 @@
 # init pygtpnl imports
 
-from .gtpclasses import V0, V1, VERSIONS, IN_ADDR, GTPTUNNEL
-from .mnlclass import SOCKADDR_NL, MNL_SOCK
+from .structures import V0, V1, VERSIONS, IN_ADDR, SOCKADDR_IN, SOCKADDR_NL, MNL_SOCK, GTPTUNNEL
 from .pygtpnl import dev_create, dev_stop, tunnel_add, tunnel_del, tunnel_list, tunnel_mod
---
+++ pygtpnl/structures.py
@@ -0,0 +1,63 @@
+'''
+Python representations of structs used in libgtpnl
+'''
+
+from ctypes import Structure, Union
+from ctypes import c_ushort, c_int, c_uint16, c_uint32, c_uint64
+from ctypes import c_byte
+
+# libgtpnl
+class V0(Structure):
+    _fields_ = [("tid", c_uint64),
+                ("flowid", c_uint16)
+               ]
+
+# libgtpnl
+class V1(Structure):
+    _fields_ = [("i_tei", c_uint32),
+                ("o_tei", c_uint32)
+               ]
+
+# libgtpnl
+class VERSIONS(Union):
+    _fields_ = [("v0", V0),
+                ("v1", V1)
+               ]
+
+# in.h
+in_addr_t = c_uint32
+class IN_ADDR(Structure):
+    _fields_ = [("s_addr", in_addr_t)
+               ]
+
+# in.h
+in_port_t = c_uint16
+sa_family_t = c_ushort
+class SOCKADDR_IN(Structure):
+    _fields_ = [("sin_family", sa_family_t),
+                ("sin_port", in_port_t),
+                ("sin_addr", IN_ADDR),
+                ("sin_zero", c_byte * 8)
+               ]
+
+sa_family_t = c_ushort
+class SOCKADDR_NL(Structure):
+      _fields_ = [("nl_family", sa_family_t),
+                  ("nl_pad", c_ushort),
+                  ("nl_pid", c_uint32),
+                  ("nl_groups", c_uint32)
+                 ]
+
+class MNL_SOCK(Structure):
+      _fields_ = [("fd", c_int),
+                  ("addr", SOCKADDR_NL)
+                 ]
+# libgtpnl
+class GTPTUNNEL(Structure):
+      _fields_ = [("ifns", c_int),
+                  ("ifidx", c_uint32),
+                  ("ms_addr", IN_ADDR),
+                  ("sgsn_addr", IN_ADDR),
+                  ("gtp_version", c_int),
+                  ("u", VERSIONS), #gtp teids
+                 ]
