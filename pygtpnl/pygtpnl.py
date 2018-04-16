05aed4abccf084c9de201f43855f6585e5553c5c
Aapo Poutanen <aapo.poutanen@aalto.fi>
2018-04-16 17:54:31 +0300

    sokid=28, func, byref fix

--- pygtpnl/pygtpnl.py
+++ pygtpnl/pygtpnl.py
@@ -76,12 +76,12 @@ def tunnel_add(ns, ue_ip, enb_ip, i_tei, o_tei, devname, sock):
     logger.debug("c_sock: {}".format(c_sock))
 
     #what is this sock_id, do it in Python maybe?
-    #if_mnlsock_id = lgnl.genl_lookup_family
-    #if_mnlsock_id.argtypes = [c_void_p, c_char_p]
-    #mnlsock_id = if_mnlsock_id(byref(c_sock), devname.encode('ascii'))
+    if_mnlsock_id = lgnl.genl_lookup_family
+    if_mnlsock_id.argtypes = [c_void_p, c_char_p]
+    mnlsock_id = if_mnlsock_id(byref(c_sock), devname.encode('ascii'))
     #mnlsock_id = if_mnlsock_id(sock, devname.encode('ascii'))
-    #logger.debug("mnlsock_id: {}".format(mnlsock_id))
-    mnlsock_id=28
+    logger.debug("mnlsock_id: {}".format(mnlsock_id))
+    #mnlsock_id=28
 
     tadd = lgnl.gtp_add_tunnel
     tadd.argtypes = [c_uint16, c_void_p, c_void_p]
