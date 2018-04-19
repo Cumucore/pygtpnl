822574f17f5a49aec52e9ba393c881d1362953b1
Aapo <root@ovs-apu>
2018-04-19 13:00:10 +0300

    tester0

--- test_tun.py
+++ test_tun.py
@@ -1,16 +1,18 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 from pygtpnl import dev_create,dev_stop,tunnel_add,tunnel_del,GtpSocket
+from time import sleep
+from pyroute2 import IPRoute
 
 def test():
-    iplist = ['127.0.0.1',
-              -1,
+    iplist = ['10.0.0.1',
               '45.45.45.45',
+              '10.10.1.1',
+              '10.10.1.2',
+              '10.10.1.3',
+              '10.10.1.4',
               '1722.236.3',
-              832675283628376287,
               'hi',
-              5,
-              2352,
              ]
     devnames = ['gtp0',
                 'äöäööäö',
@@ -22,9 +24,16 @@ def test():
 #    dev_stop(name)
     s=GtpSocket()
     s.discovery()
-    for ns in iplist:
-        tunnel_add(-1, '45.45.45.45', '23.23.23.23', 234, 12, "gtp0", s)
-        tunnel_del(ns, name, name, name, s)
+    ip=IPRoute()
+    i=ip.link_lookup(ifname="gtp0")
+    try:
+        for addr in iplist:
+            ip.route('add', dst=addr+'/32', oif=i, metrics={'mtu': 1454})
+            tunnel_add(-1, addr, '23.23.23.23', 234, 12, "gtp0", s)
+    except OSError as e:
+        print("OSerror {}".format(e))
+        pass
+    sleep(123)
     s.close()
 
 if __name__ == '__main__':
